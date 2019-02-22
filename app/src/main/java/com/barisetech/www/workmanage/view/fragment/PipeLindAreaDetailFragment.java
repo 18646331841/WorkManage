96746a43c490e8ff39f5fb7bd224c85b9f2c366b
leo <1750642946@qq.com>
2019-02-22 16:55:28 +0800

    1.修改进入管线盲区详情crash问题 2.修改波形图y轴数字过大被遮挡问题

--- app/src/main/java/com/barisetech/www/workmanage/view/fragment/PipeLindAreaDetailFragment.java
+++ app/src/main/java/com/barisetech/www/workmanage/view/fragment/PipeLindAreaDetailFragment.java
@@ -6,6 +6,7 @@ import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -99,7 +100,9 @@ public class PipeLindAreaDetailFragment extends BaseFragment{
             });
         }
 
-        if (pipeLindAreaInfo.getRemark().equals(BaseConstant.DATA_REQUEST_NAME)) {
+        if (!TextUtils.isEmpty(pipeLindAreaInfo.getRemark()) && pipeLindAreaInfo.getRemark()
+                .equals(BaseConstant.DATA_REQUEST_NAME)) {
+
             getData();
         }
     }
